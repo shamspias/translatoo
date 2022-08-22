2aabcf0c927a397e0fb9f03c5e3afdb09123dfc2
shamspias <shamsuddinpias0@gmail.com>
2022-08-22 18:03:15 +0600

    translated list of text

--- practice_code/translate_text.py
+++ practice_code/translate_text.py
@@ -4,8 +4,19 @@ from pprint import pprint
 # init the Google API translator
 translator = Translator()
 
-string = input()
+string = input("Enter your text: ")
+sentences = [
+    "Hello everyone",
+    "How are you ?",
+    "Do you speak english ?",
+    "Good bye!"
+]
 
 # translate text by given src to destination
 translation = translator.translate(string, src="bn", dest="en")
 print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+# translate list of text by given src to destination
+for i in sentences:
+    translation = translator.translate(i, src="en", dest="bn")
+    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
