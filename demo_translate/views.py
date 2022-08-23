61bc284b3d00610a95cc8f924538b2be29b56f4d
shamspias <shamsuddinpias0@gmail.com>
2022-08-23 21:13:52 +0600

    fix template view

--- demo_translate/views.py
+++ demo_translate/views.py
@@ -19,17 +19,27 @@ class DocumentTranslateView(TemplateView):
         source_language = request.GET.get('source_language')
         destination_language = request.GET.get('destination_language')
 
-        if self.check_pdf(my_file):
-            translate_pdf(my_file, source_language, destination_language)
+        if my_file is not None:
+            if self.check_pdf(my_file):
+                translate_pdf(my_file, source_language, destination_language)
 
-        elif self.check_doc(my_file):
-            translate_doc(my_file, source_language, destination_language)
+            elif self.check_doc(my_file):
+                translate_doc(my_file, source_language, destination_language)
 
-        elif self.check_docx(my_file):
-            translate_docx(my_file, source_language, destination_language)
+            elif self.check_docx(my_file):
+                translate_docx(my_file, source_language, destination_language)
+
+        context = {
+            "file": my_file
+        }
+        return render(request, 'translate_files.html', context=context)
 
     def post(self, request, *args, **kwargs):
         my_file = request.GET.get('my_file')
+        context = {
+            "file": my_file
+        }
+        return render(request, 'translate_files.html', context=context)
 
     def check_pdf(self, filename):
         print('file name', filename)
