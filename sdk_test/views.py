a7970d21098c37cfb9d02948d3bd8dd36426d474
shamspias <shamsuddinpias0@gmail.com>
2022-08-23 13:51:56 +0600

    add image render

--- sdk_test/views.py
+++ sdk_test/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse  # last line below allows MyView.get
-# to return an HttpResponse object
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
 from sdk_test import utils
@@ -12,12 +11,17 @@ class ImageView(TemplateView):
     template_name = 'image.html'
 
     def get(self, request, *args, **kwargs):
-        word = request.GET.get('text', '')
+        word = request.GET.get('text', 'Demo Image')
         print(word)
         # api_key = request.get["key"]
         my_image = utils.get_image(word, api_key=settings.STABILITY_KEY)
         print(my_image)
-        return HttpResponse(my_image)
+        return render(request, 'image.html', {'image': my_image, })
 
     def post(self, request, *args, **kwargs):
-        pass
+        word = request.POST.get('text', '')
+        print(word)
+        # api_key = request.get["key"]
+        my_image = utils.get_image(word, api_key=settings.STABILITY_KEY)
+        print(my_image)
+        return render(request, 'image.html', {'image': my_image, })
