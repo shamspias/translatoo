a4b827571914f319d534070da45a6d349c153e81
shamspias <shamsuddinpias0@gmail.com>
2022-08-23 16:04:03 +0600

    template view to translate

--- demo_translate/views.py
+++ demo_translate/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class DocumentTranslateView(TemplateView):
+    """
+    Test Translate files
+    """
+    template_name = "translate_files.html"
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
