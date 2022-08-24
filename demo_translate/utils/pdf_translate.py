0ab60b29e5b18cbc09f1413919563cbaf26f4e07
shamspias <shamsuddinpias0@gmail.com>
2022-08-25 03:13:30 +0600

    fixing docx to pdf

--- demo_translate/utils/pdf_translate.py
+++ demo_translate/utils/pdf_translate.py
@@ -13,10 +13,11 @@ def doc2pdf_linux(doc, file_path):
     :param doc: path to document
     """
     path_project = '/home/ubuntu/translatoo/'
-    cmd = ['libreoffice --convert-to pdf ' + path_project + doc + ' --outdir ' + path_project + file_path]
-    # cmd = 'libreoffice --convert-to pdf'.split() + [path_project] + [doc] + ['--outdir'] + [path_project] + [file_path]
+    # cmd = ['libreoffice --convert-to pdf ' + path_project + doc + ' --outdir ' + path_project + file_path]
+    cmd = 'libreoffice --convert-to pdf'.split() + [path_project] + [doc] + ['--outdir'] + [path_project] + [file_path]
     print(cmd)
     p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    print(p)
     p.wait(timeout=1000)
     stdout, stderr = p.communicate()
     if stderr:
