1bcec4c604e448298f9b4a125bf39e8054b42575
shamspias <shamsuddinpias0@gmail.com>
2022-08-24 12:45:55 +0600

    add model to admin

--- demo_translate/admin.py
+++ demo_translate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import LanguageCode, TranslatedFile
 
 # Register your models here.
+
+admin.site.register(LanguageCode)
+admin.site.register(TranslatedFile)
