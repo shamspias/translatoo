8de5b969179f01308c9bf4725cf448f243b7e9b6
shamspias <shamsuddinpias0@gmail.com>
2022-08-24 12:46:14 +0600

    Update view with model

--- demo_translate/views.py
+++ demo_translate/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from .models import LanguageCode
+from .forms import LanguageModelForm
 from .utils import (
     translate_docx,
     translate_doc,
     translate_pdf,
 )
-from six import BytesIO
 
 
 class DocumentTranslateView(TemplateView):
@@ -15,9 +17,19 @@ class DocumentTranslateView(TemplateView):
     template_name = "translate_files.html"
 
     def get(self, request, *args, **kwargs):
+
+        language_code = LanguageModelForm()
+        context = {
+            "languages": language_code
+        }
+        return render(request, 'translate_files.html', context=context)
+
+    def post(self, request, *args, **kwargs):
         my_file = request.GET.get('my_file')
         source_language = request.GET.get('source_language')
         destination_language = request.GET.get('destination_language')
+        print(source_language)
+        print(destination_language)
 
         if my_file is not None:
             if self.check_pdf(my_file):
@@ -28,14 +40,6 @@ class DocumentTranslateView(TemplateView):
 
             elif self.check_docx(my_file):
                 translate_docx(my_file, source_language, destination_language)
-
-        context = {
-            "file": my_file
-        }
-        return render(request, 'translate_files.html', context=context)
-
-    def post(self, request, *args, **kwargs):
-        my_file = request.GET.get('my_file')
         context = {
             "file": my_file
         }
