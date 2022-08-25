e8b6be82157831441a4b4c6139391fde48fd739d
shamspias <shamsuddinpias0@gmail.com>
2022-08-25 11:10:25 +0600

    fixed pdf translate[7]

--- demo_translate/utils/pdf_translate.py
+++ demo_translate/utils/pdf_translate.py
@@ -40,14 +40,14 @@ def translate_pdf(pdf_file, source_ln, target_ln):
     language_translation(word_file, target_word_file, source_ln, target_ln)
 
     new_pdf_file_name = "media/files/" + target_ln + "_" + pdf_file_name
-    return_pdf_path = target_ln + "_" + pdf_file_name
     try:
         from docx2pdf import convert
         convert(target_word_file, new_pdf_file_name)
+        return_pdf_path = "files/translate/" + pdf_file_name
     except:
         doc2pdf_linux(target_word_file)
+        return_pdf_path = "files/" + target_ln + "_" + pdf_file_name
 
     os.remove(word_file)
     os.remove(target_word_file)
-    file = "files/" + return_pdf_path
-    return file
+    return return_pdf_path
