c226786adff63ee34f2bbcbb3d3581dfb59e5aee
shamspias <shamsuddinpias0@gmail.com>
2022-09-05 07:36:29 +0000

    automated blog demo[2] fixed context

--- aiblog/utils.py
+++ aiblog/utils.py
@@ -4,44 +4,50 @@ import openai
 openai.api_key = settings.OPEN_AI_KEY
 
 
-def generate_blog_topics(prompt1):
+def generate_blog_topics(prompt):
+    context = {}
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Generate blog topics on: {}. \n \n 1.  ".format(prompt1),
+        prompt="Generate blog topics on: {}. \n \n 1.  ".format(prompt),
         temperature=0.7,
         max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
+    context['input'] = prompt
+    context['correction'] = response['choices'][0]['text']
+    return context
 
-    return response['choices'][0]['text']
 
-
-def generate_blog_sections(prompt1):
+def generate_blog_sections(prompt):
+    context = {}
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt1),
+        prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt),
         temperature=0.6,
         max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
+    context['input'] = prompt
+    context['correction'] = response['choices'][0]['text']
+    return context
 
-    return response['choices'][0]['text']
 
-
-def blog_section_expander(prompt1):
+def blog_section_expander(prompt):
+    context = {}
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(
-            prompt1),
+            prompt),
         temperature=0.7,
         max_tokens=200,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-
-    return response['choices'][0]['text']
+    context['input'] = prompt
+    context['correction'] = response['choices'][0]['text']
+    return context
