6a5caa45e6389673863201d2fb5c6818d7a26661
shamspias <shamsuddinpias0@gmail.com>
2022-09-05 18:33:48 +0000

    practise translate with deep translation

--- practice_code/doc_translate.py
+++ practice_code/doc_translate.py
@@ -1,5 +1,7 @@
 import pypandoc
+from bs4 import BeautifulSoup
 import re
+
 from deep_translator import (GoogleTranslator,
                              MicrosoftTranslator,
                              PonsTranslator,
@@ -17,6 +19,7 @@ def translate_language(source, ext):
     document_name = source + "." + ext
     output = pypandoc.convert_file(document_name, 'html5')
     my_string = re.findall(r'>(.+?)<', output)
+    my_string_old = my_string.copy()
     print(my_string)
     # translator = Translator()
     for i, para in enumerate(my_string):
@@ -27,6 +30,15 @@ def translate_language(source, ext):
             print("Error")
     print("-----------------------")
     print(my_string)
+    print("-----------------------")
+    soup = BeautifulSoup(output, features="lxml")
+    print("-----------------------")
+    target = soup.find_all(text=r'>(.+?)<')
+    print(target)
+    print("-----------------------")
+    for v in target:
+        v.replace_with(v.replace(my_string_old[v], my_string[v]))
+    print(soup)
 
 
 print("Please enter source document name.")
