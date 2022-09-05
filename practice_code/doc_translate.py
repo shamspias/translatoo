1321307e075530fe9f331b27c527c5097739b6b5
shamspias <shamsuddinpias0@gmail.com>
2022-09-05 16:54:43 +0000

    practise translate with deep translation

--- practice_code/doc_translate.py
+++ practice_code/doc_translate.py
@@ -1,7 +1,7 @@
 import pypandoc
 import re
 from googletrans import Translator
-
+import requests
 
 
 def translate_language(source, ext):
@@ -9,13 +9,24 @@ def translate_language(source, ext):
     output = pypandoc.convert_file(document_name, 'html5')
     my_string = re.findall(r'>(.+?)<', output)
     print(my_string)
-    translator = Translator()
+    # translator = Translator()
     for i, para in enumerate(my_string):
-        try:
-            translation = translator.translate(para, dest="bn")
-            my_string[i] = translation.text
-        except:
-            print("Error " + str(i))
+        data = {
+            "source": "auto",
+            "target": "de",
+            "text": para,
+            "proxies": [],
+        }
+        response = requests.post('https://deep-translator-api.azurewebsites.net/google/', json=data)
+        if response.json()["translation"] is None:
+            continue
+        my_string[i] = response.json()["translation"]
+    # for i, para in enumerate(my_string):
+    #     try:
+    #         translation = translator.translate(para, dest="bn")
+    #         my_string[i] = translation.text
+    #     except:
+    #         print("Error " + str(i))
     print(my_string)
 
 
