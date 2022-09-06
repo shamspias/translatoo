adc57dab75db61b991519371f9bf05f7108a1222
shamspias <shamsuddinpias0@gmail.com>
2022-09-06 16:54:55 +0000

    update translate process

--- demo_translate/utils/translate_core.py
+++ demo_translate/utils/translate_core.py
@@ -1,9 +1,21 @@
-import docx
-from googletrans import Translator
-import time
+import pypandoc
+from bs4 import BeautifulSoup
+import re
+import os
+from deep_translator import (GoogleTranslator,
+                             MicrosoftTranslator,
+                             PonsTranslator,
+                             LingueeTranslator,
+                             MyMemoryTranslator,
+                             YandexTranslator,
+                             PapagoTranslator,
+                             DeeplTranslator,
+                             QcriTranslator,
+                             single_detection,
+                             batch_detection)
 
 
-def language_translation(source_doc, target_doc, source_lan="en", target_lan="de"):
+def language_translation(source_doc, target_doc, source_lan="auto", target_lan="de"):
     """
     To translate Language
     return docx file
@@ -13,19 +25,34 @@ def language_translation(source_doc, target_doc, source_lan="en", target_lan="de
     target = target_doc.strip()
     target_language = target_lan.strip()
 
-    doc = docx.Document(source)
-    paragraphs = [para.text for para in doc.paragraphs]
-    print(len(paragraphs))
-    translator = Translator()
-    doc = docx.Document()
-    for i, para in enumerate(paragraphs):
+    output = pypandoc.convert_file(source, 'html5')
+
+    soup = BeautifulSoup(output, features="lxml")
+
+    target_tags = re.findall(r'<.+?>', output)  # get all html tags
+    target_tags = list(dict.fromkeys(target_tags))  # removing duplicates
+
+    for i, word in enumerate(target_tags):
+        target_tags[i] = word.replace('</', '<')
+        target_tags[i] = target_tags[i].replace('<', '')
+        target_tags[i] = target_tags[i].replace('>', '')
+
+        if " " in target_tags[i]:
+            target_tags[i] = target_tags[i][:target_tags[i].index(" ")]
+
+    target_tags = list(dict.fromkeys(target_tags))  # removing duplicates
+
+    for i in soup.findAll(target_tags):
         try:
-            translation = translator.translate(para, src=source_language, dest=target_language)
-            doc.add_paragraph(translation.text)
-            time.sleep(1)
-            print("Success " + str(i))
+            i.string.replace_with(GoogleTranslator(source=source_language, target=target_language).translate(i.string))
         except:
-            print("Error " + str(i))
-    doc.save(target)
+            print("")
+
+    with open("output1.html", "w") as file:
+        file.write(str(soup))
+
+    pypandoc.convert_file('output1.html', 'docx', outputfile=target)
+    os.remove("output1.html")
+
     print("Document translation is completed.")
     return True
